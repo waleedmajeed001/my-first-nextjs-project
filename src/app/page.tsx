813d4005dfd9d694220ce92148dcfcd26bd3afd8
waleedmajeed001 <147373175+waleedmajeed001@users.noreply.github.com>
2024-03-23 09:56:57 +0500

    my-frist-nextjs-project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
 function HomePage() {
-  return <h1>Hello, I am Waleed Majeed and this is my first Next.js project.</h1>
+  return <h1>Hello, I am Zulfiqar Majeed and this is my first Next.js project.</h1>
 }
 export default HomePage;
